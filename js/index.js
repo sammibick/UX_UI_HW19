90787ef575d3cac9e2b16cf2d03443f8d7067c91
sammibick <sbickford99@gmail.com>
2023-04-30 22:13:12 -0500

    Update
    update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 console.log("your index.js file is loaded correctly!");
-
-    $("v10_89").on({
+/*
+$(document).ready(function(){
+    $(".HOPE").on({
       mouseenter: function(){
-        $(this).css("background-color", "rgba(44,0,0,1)");
+        $(this).css("background-color", "lightblue");
       },  
       mouseleave: function(){
         $(this).css("background-color", "white");
@@ -11,3 +12,36 @@ console.log("your index.js file is loaded correctly!");
         $(this).css("background-color", "yellow");
       }  
     });
+  });
+*/
+
+/*
+
+
+$('.HOPE').hover(function () {
+    $(this).addClass('newColor');
+},
+function () {
+    $(this).removeClass('newColor');
+});
+
+
+
+*/
+
+
+
+
+$('.v10_93, .v10_89 ').hover(function () {
+    $('.v10_93, .v10_89').addClass('newColor');
+},
+function () {
+    $('.v10_93, .v10_89').removeClass('newColor');
+});
+
+$('.v10_114, .v10_113 ').hover(function () {
+    $('.v10_114, .v10_113').addClass('newColor');
+},
+function () {
+    $('.v10_114, .v10_113').removeClass('newColor');
+});
